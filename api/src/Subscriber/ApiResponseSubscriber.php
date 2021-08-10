6be21cdcf27a34229e9b6aa6c2f514b16b6224a2
M.Dasaev <kression@yandex.ru>
2021-08-10 17:42:02 +0000

    feat(subscriber-response): casted responses to API standart

---
+++ api/src/Subscriber/ApiResponseSubscriber.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace App\Subscriber;
+
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Paginator;
+use ApiPlatform\Core\Exception\InvalidArgumentException;
+use JetBrains\PhpStorm\ArrayShape;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Serializer\Exception\NotEncodableValueException;
+
+class ApiResponseSubscriber implements EventSubscriberInterface
+{
+    private int $totalItems;
+
+    private array $excludedRoutes = [
+        'api_doc',
+        'app_swaggerui_docs',
+    ];
+
+
+    #[ArrayShape([KernelEvents::VIEW => "array", KernelEvents::RESPONSE => "array", KernelEvents::EXCEPTION => "array"])]
+    public static function getSubscribedEvents() : array
+    {
+        return [
+            KernelEvents::VIEW => ['onKernelView', 20],
+            KernelEvents::RESPONSE => ['onKernelResponse', 10],
+            KernelEvents::EXCEPTION => ['onKernelException', 0]
+        ];
+    }
+
+    public function onKernelView(ViewEvent $event) : void
+    {
+        $paginator = $event->getControllerResult();
+        if ($paginator instanceof Paginator) {
+            $this->totalItems = $paginator->getTotalItems();
+        }
+    }
+
+    public function onKernelResponse(ResponseEvent $event) : void
+    {
+        $context = $event->getRequest()->attributes->get('_api_normalization_context');
+        $route = $event->getRequest()->attributes->get('_route');
+
+        if (!in_array($route, $this->excludedRoutes)) {
+            $responseObject = $event->getResponse();
+            $statusCode = $responseObject->getStatusCode();
+            if ($statusCode < 300) {
+                $content = json_decode($responseObject->getContent());
+                $response = [
+                    'status' => $statusCode,
+                    'data' => $content
+                ];
+                if ('collection' === $context['operation_type'] && 'get' === $context['collection_operation_name']) {
+                    $response['meta']['total'] = $this->totalItems;
+                }
+
+                $responseObject->setContent(json_encode($response));
+            } elseif ($statusCode > 399 && $statusCode < 500 && $statusCode !== 404) {
+                $content = json_decode($responseObject->getContent());
+                $errorDetails = [];
+                foreach ($content->violations as $violation) {
+                    $errorDetails[] = [
+                        'field' => $violation->propertyPath,
+                        'issue' => $violation->message
+                    ];
+                }
+
+                $error = [
+                    'name' => 'validation_error',
+                    'message' => 'An error occurred',
+                ];
+
+                if (!empty($errorDetails)) {
+                    $error['details'] = $errorDetails;
+                }
+
+                $response = [
+                    'status' => $statusCode,
+                    'error' => $error
+                ];
+
+                $responseObject->setContent(json_encode($response));
+            }
+        }
+    }
+
+    public function onKernelException(ExceptionEvent $event)
+    {
+        $exception = $event->getThrowable();
+        $response = new JsonResponse();
+
+        if ($exception instanceof NotFoundHttpException) {
+            $response->setStatusCode(404);
+            $response->setData(
+                [
+                    'status' => 404,
+                    'error' => [
+                        'name' => 'not_found',
+                        'message' => $exception->getMessage(),
+                    ],
+                ]
+            );
+        } elseif ($exception instanceof NotEncodableValueException) {
+            $response->setStatusCode(400);
+            $response->setData(
+                [
+                    'status' => 400,
+                    'error' => [
+                        'name' => 'syntax_error',
+                        'message' => 'JSON syntax error',
+                    ],
+                ]
+            );
+        } elseif ($exception instanceof InvalidArgumentException) {
+            $response->setStatusCode(400);
+            $response->setData(
+                [
+                    'status' => 400,
+                    'error' => [
+                        'name' => 'invalid_type',
+                        'message' => $exception->getMessage(),
+                    ],
+                ]
+            );
+        } elseif ($exception instanceof HttpExceptionInterface) {
+            $response->setStatusCode($exception->getStatusCode());
+            $response->headers->replace($exception->getHeaders());
+        } else {
+            $response->setStatusCode(500);
+            $response->setData(
+                [
+                    'status' => 500,
+                    'error' => [
+                        'name' => 'server_error',
+                        'message' => $exception->getMessage(),
+                    ],
+                ]
+            );
+        }
+
+        $event->setResponse($response);
+    }
+}
